76a0ad1bff17bbf64e624bd944f61afff52efdaf
maci7-14-01 <maci71401@maci7-14-01s-MacBook-Pro.local>
2025-04-29 11:55:30 +0530

    Third task commit

--- sarvadhi_custom/sarvadhi_custom/doctype/town_hall_event/town_hall_event.js
+++ sarvadhi_custom/sarvadhi_custom/doctype/town_hall_event/town_hall_event.js
@@ -6,3 +6,17 @@
 
 // 	},
 // });
+frappe.ui.form.on('Town Hall Event', {
+    refresh(frm) {
+        // Show the custom button only if the document is not new
+        if (!frm.is_new()) {
+            frm.add_custom_button(__('View Registrations'), function () {
+                const eventName = frm.doc.name;
+
+                frappe.set_route('List', 'Town Hall Event Registration', {
+                    town_hall_event: eventName
+                });
+            }, __('Actions'));
+        }
+    }
+});
